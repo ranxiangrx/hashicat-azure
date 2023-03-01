09a332212a1943f5d4501bd42da2f7508df966e4
ranxiangrx <ran-xiang@hotmail.com>
2023-03-01 21:34:20 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -114,7 +114,6 @@ resource "azurerm_linux_virtual_machine" "catapp" {
   admin_username                  = var.admin_username
   admin_password                  = var.admin_password
   disable_password_authentication = false
-  Department			  = devops
   network_interface_ids           = [azurerm_network_interface.catapp-nic.id]
 
   source_image_reference {
