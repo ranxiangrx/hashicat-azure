3f307ab3832baebc8bf51770ebd376eb85331ede
ranxiangrx <ran-xiang@hotmail.com>
2023-03-01 21:50:19 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "azurerm_linux_virtual_machine" "catapp" {
 
   tags = {
 	Department = "devops"
-	Billable   = "true"
+#	Billable   = "true"
 }
 
   # Added to allow destroy to work correctly.
